131f663515cf05f1d24830a64f531cb2c2630fb2
Duane Johnson <duane.johnson@gmail.com>
2020-05-14 18:43:59 -0600

    Play with lighting

--- src/has_scene.js
+++ src/has_scene.js
@@ -4,10 +4,11 @@ const { Scene, Fog, Color, AmbientLight, DirectionalLight } = THREE
 
 const FOG_DEFAULT_COLOR = 0xFFFFFF
 const FOG_DEFAULT_NEAR_DISTANCE = 6600
-const FOG_DEFAULT_FAR_DISTANCE = 7600
+const FOG_DEFAULT_FAR_DISTANCE = 8600
 
-const LIGHT_COLOR = 0xFFFFFF
+const LIGHT_AMBIENT_COLOR = 0xF0F0F0
 const LIGHT_AMBIENT_FACTOR = 0.76666
+const LIGHT_DIRECTIONAL_COLOR = 0x020202
 const LIGHT_DIRECTIONAL_FACTOR = 0.48
 
 const HasScene = stampit({
@@ -29,8 +30,8 @@ const HasScene = stampit({
     this.scene = scene
     this.fog = fog
 
-    this.ambientLight = new AmbientLight(LIGHT_COLOR, LIGHT_AMBIENT_FACTOR)
-    this.directionalLight = new DirectionalLight(LIGHT_COLOR, LIGHT_DIRECTIONAL_FACTOR)
+    this.ambientLight = new AmbientLight(LIGHT_AMBIENT_COLOR, LIGHT_AMBIENT_FACTOR)
+    this.directionalLight = new DirectionalLight(LIGHT_DIRECTIONAL_COLOR, LIGHT_DIRECTIONAL_FACTOR)
 
     scene.fog = new Fog(new Color(this.fog.color), this.fog.near, this.fog.far)
     scene.add(this.ambientLight)
