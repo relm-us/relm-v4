d8e96c2bd5f5563294ef2c15a179b74f901b4fcf
Duane Johnson <duane.johnson@gmail.com>
2020-06-20 10:57:36 -0600

    Add player bounds check to Ground

--- src/ground.js
+++ src/ground.js
@@ -10,6 +10,11 @@ import { AnimatesPosition } from './components/animates_position.js'
 import { defineGoal } from './goals/goal.js'
 import { RoughCircleBufferGeometry } from './geometries/rough_circle_geometry.js'
 
+const GROUND_ROUGH_CIRCLE_VARIATION = 20
+const GROUND_BOUNDS_BUFFER_SIZE = 150
+const GROUND_BOUNDS_SMIDGE = 25
+const GROUND_ADD_POSITION = 100
+
 const UsesAssetAsGround = stampit(Component, {
   deepStatics: {
     goalDefinitions: {
@@ -47,14 +52,15 @@ const UsesAssetAsGround = stampit(Component, {
     },
     
     _createGeometry() {
+      const groundType = this.goals.ground.get('type')
       const groundSize = this.goals.ground.get('size')
       const randomSeed = this.goals.ground.get('seed')
-      switch (this.goals.ground.get('type')) {
+      switch (groundType) {
         case 'circle': return new THREE.CircleBufferGeometry(groundSize / 2, 60)
-        case 'rough': return new RoughCircleBufferGeometry(groundSize / 2, 60, groundSize / 20, randomSeed)
+        case 'rough': return new RoughCircleBufferGeometry(groundSize / 2, 60, this._roughCircleVariation(), randomSeed)
         case 'square': return new THREE.PlaneBufferGeometry(groundSize, groundSize)
         default:
-          return new THREE.PlaneBufferGeometry(groundSize, groundSize)
+          console.error('Ground type not recognized', groundType)
       }
     },
 
@@ -87,12 +93,55 @@ const UsesAssetAsGround = stampit(Component, {
       this.object.add(this.mesh)
       this.emit('mesh-updated')
     },
+
+    _roughCircleVariation() {
+      return this.goals.ground.get('size') / GROUND_ROUGH_CIRCLE_VARIATION
+    },
+    
+    _keepCircleBounds(player, distance, radius) {
+      if (distance > radius && distance <= radius + GROUND_BOUNDS_BUFFER_SIZE) {
+        const vec = new THREE.Vector3()
+        vec.copy(this.object.position)
+        vec.sub(player.object.position)
+        vec.normalize()
+        vec.multiplyScalar(GROUND_ADD_POSITION)
+        player.addPosition(vec)
+      }
+    },
+
+    _keepSquareBounds(player, halfsize) {
+      const dx = player.object.position.x - this.object.position.x
+      const dz = player.object.position.z - this.object.position.z
+      if (dx > halfsize && dx < halfsize + GROUND_BOUNDS_BUFFER_SIZE) {
+        player.addPosition(new THREE.Vector3(-GROUND_ADD_POSITION, 0, 0))
+      } else if (dx < -halfsize && dx > -halfsize - GROUND_BOUNDS_BUFFER_SIZE) {
+        player.addPosition(new THREE.Vector3(GROUND_ADD_POSITION, 0, 0))
+      }
+      if (dz > halfsize && dz < halfsize + GROUND_BOUNDS_BUFFER_SIZE) {
+        player.addPosition(new THREE.Vector3(0, 0, -GROUND_ADD_POSITION))
+      } else if (dz < -halfsize && dz > -halfsize - GROUND_BOUNDS_BUFFER_SIZE) {
+        player.addPosition(new THREE.Vector3(0, 0, GROUND_ADD_POSITION))
+      }
+      
+    },
     
     update(_delta) {
       const groundGoal = this.goals.ground
       if (!groundGoal.achieved) {
         this._createGroundMeshFromLoadedTexture(this.texture)
         groundGoal.markAchieved()
+      }
+      
+      const player = this.stage.player
+      if (!player || this.stage.editorMode) { return }
+      
+      // Prevent the player from leaving the ground's boundaries
+      const distance = this.object.position.distanceTo(player.object.position)
+      const radius = groundGoal.get('size')  / 2
+      switch (groundGoal.get('type')) {
+        case 'circle': return this._keepCircleBounds(player, distance, radius - GROUND_BOUNDS_SMIDGE)
+        case 'rough': return this._keepCircleBounds(player, distance, radius - this._roughCircleVariation())
+        case 'square': return this._keepSquareBounds(player, radius - GROUND_BOUNDS_SMIDGE)
       }
     }
   }
