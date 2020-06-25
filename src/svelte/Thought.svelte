87b465920a816713d35a9df5867c19a7bad60343
Duane Johnson <duane.johnson@gmail.com>
2020-06-25 13:17:35 -0600

    Better documentation. Mention '/share'.

---
+++ src/svelte/Thought.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let text = null
+</script>
+
+<style>
+  .thought {
+    opacity: 0.8;
+    margin: 10px 0;
+    padding: 4px 10px;
+    background-color: #555;
+    display: flex;
+  }
+  .thought > input {
+    font-size: 24px;
+    border: 2px solid #111;
+    color: white;
+    background-color: #555;
+    padding: 4px 10px;
+    flex-grow: 1;
+  }
+  .thought > input::placeholder {
+    color: white;
+  }
+</style>
+
+
+<div class="thought">
+  <input placeholder="What's on your mind? ..." value={ text } disabled>
+</div>
