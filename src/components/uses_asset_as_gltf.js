0f749358bf46d295e2fdc14412994b0984fa724e
Duane Johnson <duane.johnson@gmail.com>
2020-07-07 07:16:14 -0600

    Center group meshes on 3d objects

--- src/components/uses_asset_as_gltf.js
+++ src/components/uses_asset_as_gltf.js
@@ -35,7 +35,7 @@ const UsesAssetAsGltf = stampit(Component, {
         this.child = findFirstMesh(gltf.scene)
         if (this.child) {
           this.child.scale.set(1, 1, 1)
-          this.child.position.set(0, 0, 0)
+          this._centerGltf(this.child)
           this.object.add(this.child)
         } else {
           console.warn("Couldn't find first mesh in GLTF scene", gltf.scene)
@@ -44,7 +44,35 @@ const UsesAssetAsGltf = stampit(Component, {
         this.child = null
       }
     },
+
+    _centerGltf(object3d) {
+      object3d.position.set(0, 0, 0)
+      if (object3d.type === 'Group') {
+        const meshes = object3d.children.filter(child => child.type === 'Mesh')
+        const center = this._getCenter(meshes)
+        for (const child of meshes) {
+          child.position.x -= center.x
+          child.position.y -= center.y
+          child.position.z -= center.z
+        }
+      }
+    },
     
+    _getCenter(objects) {
+      const center = {x: 0, y: 0, z: 0}
+      const size = objects.length
+      for (const obj of objects) {
+        center.x += obj.position.x
+        center.y += obj.position.y
+        center.z += obj.position.z
+      }
+      center.x /= size
+      center.y /= size
+      center.z /= size
+
+      return center
+    },
+
     update(_delta) {
       const normScaleGoal = this.goals.normalizedScale
       if (!normScaleGoal.achieved && this.child) {
