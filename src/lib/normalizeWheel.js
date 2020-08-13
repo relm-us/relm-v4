e3e677ed0d1ef0551c9bd7cac5261a0ac681e5c2
Duane Johnson <duane.johnson@gmail.com>
2020-08-13 16:09:42 -0600

    Test wheel units for zoom

--- src/lib/normalizeWheel.js
+++ src/lib/normalizeWheel.js
@@ -16,10 +16,10 @@ function normalizeWheel(event) {
       pixelY = MathUtils.clamp(event.deltaY * PIXEL_STEP, -20, 20)
       break
     case DELTA_MODE_LINE:
-      pixelY = MathUtils.clamp(event.deltaY * PIXEL_STEP, -2, 2)
+      pixelY = MathUtils.clamp(event.deltaY * PIXEL_STEP, -20, 20)
       break
     case DELTA_MODE_PAGE:
-      pixelY = MathUtils.clamp(event.deltaY * PIXEL_STEP, -2, 2)
+      pixelY = MathUtils.clamp(event.deltaY * PIXEL_STEP, -20, 20)
       break
   }
   return pixelY
