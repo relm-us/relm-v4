f4cdab2a4dce48d39b538f389f4e8142b8ce675a
Duane Johnson <duane.johnson@gmail.com>
2020-08-15 08:11:49 -0600

    Change 'Save' button to 'Apply Changes' in editor

--- src/svelte/ExportImport.svelte
+++ src/svelte/ExportImport.svelte
@@ -1,31 +1,29 @@
 <script>
   import { exportImportState } from './stores.js'
-  
+
   import { config } from '../config.js'
   import { showToast } from '../lib/Toast.js'
   import { exportRelm, importRelm } from '../export.js'
-  
+
   export let stage
   export let network
 
   const cfg = config(window.location)
-  
+
   let isOpen
   let text
 
-  exportImportState.subscribe(value => {
+  exportImportState.subscribe((value) => {
     isOpen = value
   })
-  
-  
+
   function clearText() {
     text = ''
   }
-  
+
   function exportSelected() {
     text = JSON.stringify(exportRelm(stage, network, true, cfg.ROOM), null, 2)
   }
-
 
   function closeWindow() {
     exportImportState.update(() => false)
@@ -44,87 +42,89 @@
       showToast(`Unable to parse JSON: ${err}`)
       return
     }
-    
+
     const objectCount = importRelm(network, data)
     showToast(`Imported ${objectCount} objects into this relm.`)
-    
+
     handleClose()
     event.stopPropagation()
   }
-  
+
   function handleKeydown(event) {
     if (isOpen && event.keyCode === 27) {
       handleClose()
       event.stopPropagation()
     }
   }
-  
+
   $: if (isOpen) {
     exportSelected()
   }
 </script>
 
-
 <style>
-.import-export {
-  display: flex;
-  flex-direction: column;
-  position: fixed;
-  top: 80px;
-  right: 10%;
-  bottom: 80px;
-  left: 10%;
-  z-index: 4;
-  background-color: #fff;
-}
-textarea {
-  resize: none;
-  flex-grow: 1;
-  padding: 15px;
-  border: none;
-  border-bottom: 2px solid #999;
-}
-.import-export > .button-panel {
-  margin: 10px 15px;
-  display: flex;
-  justify-content: space-between;
-}
+  .import-export {
+    display: flex;
+    flex-direction: column;
+    position: fixed;
+    top: 80px;
+    right: 10%;
+    bottom: 80px;
+    left: 10%;
+    z-index: 4;
+    background-color: #fff;
+  }
+  textarea {
+    resize: none;
+    flex-grow: 1;
+    padding: 15px;
+    border: none;
+    border-bottom: 2px solid #999;
+  }
+  .import-export > .button-panel {
+    margin: 10px 15px;
+    display: flex;
+    justify-content: space-between;
+  }
 
-.button-panel-wrap-left {
-  display: flex;
-  justify-content: flex-start;
-}
+  .button-panel-wrap-left {
+    display: flex;
+    justify-content: flex-start;
+  }
 
-.button-panel-wrap-right {
-  display: flex;
-  justify-content: flex-end;
-}
+  .button-panel-wrap-right {
+    display: flex;
+    justify-content: flex-end;
+  }
 
-.button {
-  margin-left: 10px;
-}
+  .button {
+    margin-left: 10px;
+  }
 </style>
 
-<svelte:window on:keydown|capture={handleKeydown}/>
+<svelte:window on:keydown|capture={handleKeydown} />
 
-<div
-  class="import-export"
-  class:hide={ !isOpen }
->
-  <textarea value={ text } on:input={ (e) => { text = e.target.value }}></textarea>
-  
+<div class="import-export" class:hide={!isOpen}>
+  <textarea
+    value={text}
+    on:input={(e) => {
+      text = e.target.value
+    }} />
+
   <div class="button-panel">
-  
+
     <div class="button-panel-wrap-left">
-      <button class="button" on:click={ clearText }>Clear Editor</button>
-      <button class="button" on:click={ exportSelected }>Reset Editor</button>
+      <button class="button" on:click={clearText}>Clear Editor</button>
+      <button class="button" on:click={exportSelected}>Reset Editor</button>
     </div>
-    
+
     <div class="button-panel-wrap-right">
-      <button class="button" on:click={ handleImport }>Save</button>
-      <button class="button" on:mouseup|capture|stopPropagation={ handleClose }>Close</button>
+      <button class="button" on:click={handleImport}>Apply Changes</button>
+      <button class="button" on:mouseup|capture|stopPropagation={handleClose}>
+        Close
+      </button>
     </div>
-  
+
   </div>
-  
+
 </div>
